56910c9e2a7856f98ba9d543f6f20042b5c5064b
Lienha Carleton <lhariver@gmail.com>
2014-09-05 22:25:37 -0700

    update tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  
+  let(:base_title) { "Sample App |"}
   describe 'Home page' do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +11,7 @@ describe "StaticPages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Sample App | Home')
+      expect(page).to have_title("#{base_title} Home")
     end
 
   end
@@ -25,7 +25,7 @@ describe "StaticPages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Sample App | Help')
+      expect(page).to have_title("#{base_title} Help")
     end
 
   end
@@ -39,8 +39,23 @@ describe "StaticPages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title('Sample App | About Us')
+      expect(page).to have_title("#{base_title} About Us")
     end
+
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} Contact Us")
+    end
+
   end
 
 end
